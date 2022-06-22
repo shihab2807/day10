1d85b71719c5c56966adffd0025a4f6f5396fa9a
ACER <ACER@DESKTOP-6D1AVH9>
2022-06-22 08:19:52 +0100

    day10 done

---
+++ day10/src/day10/demo2.java
@@ -0,0 +1,16 @@
+package day10;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+Integer a = new Integer(10);
+		
+		int j =a.intValue();
+		int i =a;
+		System.out.println(a + " "+ j + " " + i);
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
